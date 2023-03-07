7c990f73458dc47f488b9be7b34fe4e06b8dd0ee
Admin <dashko.pavel@bk.ru>
2023-03-07 21:40:46 +0300

    It was easy! Repeat! Repeat! Repeat!

--- Problem_1_8_4/Source.cpp
+++ Problem_1_8_4/Source.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+//
+// Write a power function that implements 
+// the raising of an integer to a non-negative integer power
+//
+
+int power(int x, unsigned p) 
+{
+    int answer;
+    if (p == 0) return 1;
+    return x*power(x, p - 1);
+}
+
 int main()
 {
-	std::cout << "YES!";
+    int x = 0;
+    unsigned p = 0;
+    std::cin >> x >> p;
+    std::cout << power(x, p);
 	return 0;
 }
