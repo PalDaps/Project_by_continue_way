aa64c81b67565c9ff031a2e5b09c0043daca4584
Admin <dashko.pavel@bk.ru>
2023-03-09 23:03:30 +0300

    The problem 1_8_12 done!

--- Problem_1_8_12/Source.cpp
+++ Problem_1_8_12/Source.cpp
@@ -2,7 +2,11 @@
 #include <string>
 
 //
-//
+// Write a program that reads character - by - 
+// character from std::cin until it consumes 
+// the input stream, and replaces several 
+// consecutive spaces with oneand outputs 
+// the result to std::cout.
 //
 
 int main()
@@ -11,11 +15,10 @@ int main()
 	char c = '\0';
 	while (std::cin.get(c))
 	{
+		if (c != ' ' && c != '\n') count = 0;
 		if (c == ' ') count++;
-
-		std::cout << c;
+		if ( count <= 1 ) std::cout << c;
 		if (c == '\n') break;
 	}
-	std::cout << count;
 	return 0;
 }
