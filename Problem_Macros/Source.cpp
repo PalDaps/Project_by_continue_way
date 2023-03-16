df5d82b51d6bf492dd270dda124806f4b2dd2a74
Admin <dashko.pavel@bk.ru>
2023-03-16 15:36:37 +0300

    The testing of recursion

--- Problem_Macros/Source.cpp
+++ Problem_Macros/Source.cpp
@@ -1,14 +1,51 @@
 #include <iostream>
 // #define test1
-// #define recursion
+// #define recursion_io
+#define recursion_reverse
+
+void io_numbers()
+{
+	int a;
+	std::cin >> a;
+	std::cout << a << " ";
+	if (a == 0) return;
+	io_numbers();
+}
+void io_reverse_numbers(int n)
+{
+	int i;
+	i = n;
+	std::cout << i << " ";
+	n--;
+	if (n == 0)
+	{
+		std::cout << n;
+		return;
+	}
+	io_reverse_numbers(n);
+}
 
 int main()
 {
 #ifdef test1
-	for (int i = 0; i < 4; ++i)
+	int n = 0;
+	std::cin >> n;
+	for (int i = n; i >= 0; --i)
 		std::cout << i << " ";
 
 	std::cout << "HALLO";
+#endif
+
+
+#ifdef recursion_io
+	input_the_numbers();
+#endif
+
+
+#ifdef recursion_reverse
+	int n = 0;
+	std::cin >> n;
+	io_reverse_numbers(n);
 #endif
 	return 0;
 }
