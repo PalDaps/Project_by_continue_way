8490f087527db93d40e1ff5613faea09c55939ac
Admin <dashko.pavel@bk.ru>
2023-03-17 16:19:14 +0300

    The third version of problem 2_3_9

--- Problem_2_3_9/Source.cpp
+++ Problem_2_3_9/Source.cpp
@@ -8,11 +8,12 @@
 
 void rotate(int a[], unsigned size, int shift)
 {
-	for (int i = 0; i <= size; ++i)
+	int first_elem = shift % size;
+	for (first_elem; first_elem < size; ++first_elem)
 	{
-		a[(i + 1) % size] = a[i];
+		std::cout << a[first_elem] << " ";
 	}
-	for (int i = 0; i < size; ++i)
+	for (int i = 0; i < shift%size; i++)
 	{
 		std::cout << a[i] << " ";
 	}
@@ -24,7 +25,8 @@ int main()
 	unsigned size = sizeof(a) / 4;
 	int* p_1 = &a[1];
 	int* p_2 = &a[2];
-
+	int shift = 0;
+	std::cin >> shift;
 	std::cout << p_1 << " " << p_2 << std::endl;
 	p_1++;
 	std::cout << p_1 << std::endl;
@@ -35,6 +37,6 @@ int main()
 	for (int i = 0; i < size; ++i)
 		std::cout << a[i] << " ";
 	std::cout << std::endl;
-	rotate(a, size, 2);
+	rotate(a, size, shift);
 	return 0;
 }
