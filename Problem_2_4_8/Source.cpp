2d1ff18deab1e9bd98e6e7847b6c6a16b4fe8d25
Admin <dashko.pavel@bk.ru>
2023-03-18 22:45:44 +0300

    The second version of problem 2_4_8

--- Problem_2_4_8/Source.cpp
+++ Problem_2_4_8/Source.cpp
@@ -9,13 +9,25 @@
 //
 int strstr_mine(const char* text, const char* pattern)
 {
-	int counter = 0;
+	int counter = 0, length = 0;
+	const char* ptr_safe_text = 0;
+	const char* ptr_safe_pattern = 0;
 	const char* ptr_first_text = &text[0];
 	const char* ptr_first_pattern = &pattern[0];
+	if ( *ptr_first_pattern == '\0') return 0;
+	/* while (*ptr_first_pattern != '\0')
+	{
+		ptr_first_pattern++;
+		length++;
+		}
+	*/ 
+	ptr_first_pattern = &pattern[0];
 	for (ptr_first_text; *ptr_first_text != '\0'; ptr_first_text++)
 	{
 		if (*ptr_first_text == *ptr_first_pattern)
 		{
+			ptr_safe_text = ptr_first_text;
+			ptr_safe_pattern = ptr_first_pattern;
 			counter++;
 			ptr_first_text++;
 			ptr_first_pattern++;
@@ -26,15 +38,26 @@ int strstr_mine(const char* text, const char* pattern)
 					counter++;
 				}
 			}
+			ptr_first_text = ptr_safe_text;
+			ptr_first_pattern = ptr_safe_pattern;
+			break;
 		}
 	}
-
+	ptr_first_text = &text[0];
+	if (ptr_safe_text == nullptr) return -1;
+	while (*ptr_first_text != *ptr_safe_text)
+	{
+		ptr_first_text++;
+		length++;
+	}
+	return length;
 }
 
 int main()
 {
-	char text[] = "Holiadboy";
-	char pattern[] = "boy";
+	char text[] = "avavc";
+	char pattern[] = "avc";
+	std::cout << strstr_mine(text, pattern);
 
 	return 0;
 }
