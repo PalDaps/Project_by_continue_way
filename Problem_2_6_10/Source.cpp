318a4e73e5661b9f367ad23209de534fa8ac3795
Admin <dashko.pavel@bk.ru>
2023-03-25 23:50:49 +0300

    Worked with memory allocation. I understood a little what new and delete are. Most likely, there is a leak somewhere. But I decided.

--- Problem_2_6_10/Source.cpp
+++ Problem_2_6_10/Source.cpp
@@ -8,29 +8,47 @@
 // line break character ('\n'), and returns a C-style 
 // string with the characters read.
 //
-
-char* getline_mine()
-{
-
-}
-
-char* memory_oxp(char *arr_of_char, int new_size, int size, int i)
+char* memory_oxp(char* arr_of_char, int new_size, int i)
 {
 	char* more_size_arr = new char[new_size];
-	size = new_size;
 	for (int j = 0; j < i; j++)
 	{
 		more_size_arr[j] = arr_of_char[j];
 	}
 	delete[] arr_of_char;
 	arr_of_char = more_size_arr;
-
+	return arr_of_char;
 }
-
+char* getline_mine()
+{
+	char symbol = 'a';
+	int size = 10, new_size = 0;
+	char* arr_of_char = new char[size];
+	int i = 0;
+	while (std::cin.get(symbol) && symbol != '\n' && symbol != std::cin.eof())
+	{
+		if (i >= size)
+		{
+			new_size = size * 2;
+			arr_of_char = memory_oxp(arr_of_char, new_size, i);
+			size = new_size;
+		}
+		arr_of_char[i] = symbol;
+		i++;
+	}
+	arr_of_char = memory_oxp(arr_of_char, i+1, i);
+	arr_of_char[i] = '\0';
+	for (int j = 0; j < i; j++)
+	{
+		std::cout << *(arr_of_char + j);
+	}
+		return arr_of_char;
+		delete[] arr_of_char;
+}
 
 int main()
 {
-	char symbol = 'a';
+	/* char symbol = 'a';
 	int size = 10, new_size = 0;
 	char* arr_of_char = new char[size];
 	int i = 0;
@@ -57,12 +75,14 @@ int main()
 		new_arr_of_char[j] = arr_of_char[j];
 	}
 	delete[] arr_of_char;
-
+	arr_of_char = new_arr_of_char;
 	for (int j = 0; j < i; j++)
 	{
-		std::cout << *(new_arr_of_char+j);
+		std::cout << *(arr_of_char+j);
 	}
-	delete[] new_arr_of_char;
+	*/
+	getline_mine();
+	// delete[] arr_of_char;
 	
 	return 0;
 }
