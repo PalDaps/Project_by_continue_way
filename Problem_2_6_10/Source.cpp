f4b5040b62fc3f775a56910a676818e738917edc
Admin <dashko.pavel@bk.ru>
2023-03-25 22:41:07 +0300

    The getline is working

--- Problem_2_6_10/Source.cpp
+++ Problem_2_6_10/Source.cpp
@@ -17,13 +17,20 @@ int main()
 	int i = 0;
 	while (std::cin.get(symbol) && symbol != '\n')
 	{
-		arr_of_char[i] = symbol;
-		i++;
-		if (i > size)
+		if (i >= size)
 		{
 			new_size = size * 2;
 			char* more_size_arr = new char[new_size];
+			size = new_size;
+			for (int j = 0; j < i; j++)
+			{
+				more_size_arr[j] = arr_of_char[j];
+			}
+			delete[] arr_of_char;
+			arr_of_char = more_size_arr;
 		}
+		arr_of_char[i] = symbol;
+		i++;
 	}
 	char* new_arr_of_char = new char[i];
 	for (int j = 0; j < i; j++)
