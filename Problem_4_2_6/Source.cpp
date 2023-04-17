46e3ead6e4a6ef3a0acc3e4a7c15fdf16d650189
Pavel Dashko <dashko.pavel@bk.ru>
2023-04-17 21:01:54 +0300

    The testing of functions overload.

---
+++ Problem_4_2_6/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void foo(char)
+{
+std::cout << "char" << std::endl;
+}
+
+void foo(signed char)
+{
+	std::cout << "signed char" << std::endl;
+}
+
+void foo(unsigned char)
+{
+	std::cout << "unsigned char" << std::endl;
+}
+
+int main()
+{
+	foo('a');
+	return 0;
+}
