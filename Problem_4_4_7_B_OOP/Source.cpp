75f381c01429104c2d0aee7729818673d8ed5320
Pavel Dashko <dashko.pavel@bk.ru>
2023-04-28 15:44:07 +0300

    After all, it is still difficult to deal with inheritance, virtual functions, polymorphism

---
+++ Problem_4_4_7_B_OOP/Source.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+//
+// You need to implement a function that takes two pointers 
+// to the base Expression class as input, and returns true 
+// if both pointers actually point to objects of the same class, 
+// and false otherwise
+//
+class Expression
+{
+public:
+	virtual double evaluate() const = 0;
+	virtual ~Expression()
+	{
+
+	}
+};
+
+
+class Number : Expression
+{
+public:
+	Number(double value) : value(value)
+	{
+	}
+	double evaluate() const
+	{
+		return value;
+	}
+	~Number()
+	{
+		std::cout << "Destructor frome Number" << std::endl;
+	}
+
+private:
+	double value;
+};
+
+class BinaryOperation : Expression
+{
+public:
+	BinaryOperation(Expression const* left, char op, Expression const* right)
+		: left(left), op(op), right(right)
+	{
+
+	}
+	double evaluate() const
+	{
+		switch (op)
+		{
+		case '*':
+			return (*left).evaluate() * (*right).evaluate();
+		case '+':
+			return left->evaluate() + right->evaluate();
+		case '/':
+			return left->evaluate() / right->evaluate();
+		case '-':
+			return left->evaluate() - right->evaluate();
+		default:
+			return 0;
+		}
+
+	}
+	~BinaryOperation()
+	{
+		std::cout << "Destructor from BinaryOperation" << std::endl;
+		delete left;
+		delete right;
+	}
+
+private:
+	Expression const* left;
+	char op;
+	Expression const* right;
+
+};
+
+bool check_equals(Expression const* left, Expression const* right)
+{
+
+}
+
+int main()
+{
+	Number left(2.5);
+	Number right(3.0);
+	BinaryOperation* binary = new BinaryOperation(left, '+', right);
+
+	return 0;
+}
