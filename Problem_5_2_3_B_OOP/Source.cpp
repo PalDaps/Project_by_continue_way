69c45a8f32846793c9f4238dccf85aed18f9a649
Pavel Dashko <dashko.pavel@bk.ru>
2023-06-11 23:53:47 +0300

    Rational numbers

--- Problem_5_2_3_B_OOP/Source.cpp
+++ Problem_5_2_3_B_OOP/Source.cpp
@@ -12,23 +12,69 @@ struct Rational {
 		std::cout << "The destructor volunteered" << std::endl;
 	}
 	void add(Rational number);
+	void sub(Rational number);
+	void mul(Rational number);
+	void div(Rational number);
+	int gcd(int a, int b);
 };
+
+int Rational::gcd(int a, int b) {
+	a = std::abs(a);
+	b = std::abs(b);
+	int ost = 1;
+	if (a > b)
+		std::swap(a, b);
+	while (ost != 0) {
+		ost = b % a;
+		b = a;
+		a = ost;
+	}
+	return b;
+}
 
 
 void Rational::add(Rational number) {
-	int temp_numerator = number.numerator_;
-	int temp_denominator = number.denominator_;
-	number.numerator_ = number.numerator_ * denominator_;
-	number.denominator_ = number.denominator_ * denominator_;
-	numerator_ = numerator_ * number.denominator_;
+	numerator_ = numerator_ * number.denominator_ + number.numerator_ * denominator_;
 	denominator_ = denominator_ * number.denominator_;
+	int del = gcd(numerator_, denominator_);
+	numerator_ = numerator_ / del;
+	denominator_ = denominator_ / del;
 }
+
+void Rational::sub(Rational number) {
+	numerator_ = numerator_ * number.denominator_ - number.numerator_ * denominator_;
+	denominator_ = denominator_ * number.denominator_;
+	int del = gcd(numerator_, denominator_);
+	numerator_ = numerator_ / del;
+	denominator_ = denominator_ / del;
+}
+
+void Rational::mul(Rational number) {
+	numerator_ = numerator_ * number.numerator_;
+	denominator_ = denominator_ * number.denominator_;
+	int del = gcd(numerator_, denominator_);
+	numerator_ = numerator_ / del;
+	denominator_ = denominator_ / del;
+}
+
+void Rational::div(Rational number) {
+	numerator_ = numerator_ * number.denominator_;
+	denominator_ = denominator_ * number.numerator_;
+	int del = gcd(numerator_, denominator_);
+	numerator_ = numerator_ / del;
+	denominator_ = denominator_ / del;
+}
+
+
 
 int main() {
 	std::cout << "HELLO!" << std::endl;
-	Rational number1(3, 2);
-	Rational number2(7, 2);
-	number1.add(number2);
+	Rational number1(5, 1);
+	Rational number2(2, 1);
+	// number1.add(number2);
+	// number1.sub(number2);
+	// number1.mul(number2);
+	number1.div(number2);
 	std::cout << "Numerator_ of number1 : " << number1.numerator_ << std::endl;
 	std::cout << "Denominator_ of number1 : " << number1.denominator_ << std::endl;
 	return 0;
