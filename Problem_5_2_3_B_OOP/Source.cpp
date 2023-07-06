2fdbdeb4847e3da3ff7852f3f1c83f966341163c
Pavel Dashko <dashko.pavel@bk.ru>
2023-07-06 18:29:01 +0300

    I'm trying to fix the code

--- Problem_5_2_3_B_OOP/Source.cpp
+++ Problem_5_2_3_B_OOP/Source.cpp
@@ -19,8 +19,8 @@ struct Rational {
 	void mul(const int number);
 	void div(Rational number);
 	void div(const int number);
-	const Rational& operator+();
-	const Rational& operator-();
+	Rational& operator+() const;
+	Rational& operator-() const;
 	Rational& operator+=(const Rational& another);
 	Rational& operator+=(const int another);
 	Rational& operator-=(const Rational& another);
@@ -30,8 +30,7 @@ struct Rational {
 	Rational& operator/=(const Rational& another);
 	Rational& operator/=(const int another);
 	int gcd(int a, int b);
-
-
+	void neg() { numerator_ = -numerator_; };
 };
 
 int Rational::gcd(int a, int b) {
@@ -57,9 +56,9 @@ void Rational::add(Rational number) {
 	denominator_ = denominator_ / del;
 }
 
-void Rational::add(const int another) {
-	numerator_ = numerator_ * number.denominator_ + number.numerator_ * denominator_;
-	denominator_ = denominator_ * number.denominator_;
+void Rational::add(const int number) {
+	numerator_ = numerator_ + denominator_ * number;
+	denominator_ = denominator_ * number;
 	int del = gcd(numerator_, denominator_);
 	numerator_ = numerator_ / del;
 	denominator_ = denominator_ / del;
@@ -73,9 +72,24 @@ void Rational::sub(Rational number) {
 	denominator_ = denominator_ / del;
 }
 
+void Rational::sub(const int number) {
+	numerator_ = numerator_ - number * denominator_;
+	denominator_ = denominator_ *number;
+	int del = gcd(numerator_, denominator_);
+	numerator_ = numerator_ / del;
+	denominator_ = denominator_ / del;
+}
+
 void Rational::mul(Rational number) {
 	numerator_ = numerator_ * number.numerator_;
 	denominator_ = denominator_ * number.denominator_;
+	int del = gcd(numerator_, denominator_);
+	numerator_ = numerator_ / del;
+	denominator_ = denominator_ / del;
+}
+
+void Rational::mul(const int number) {
+	numerator_ = numerator_ * number;
 	int del = gcd(numerator_, denominator_);
 	numerator_ = numerator_ / del;
 	denominator_ = denominator_ / del;
@@ -89,13 +103,22 @@ void Rational::div(Rational number) {
 	denominator_ = denominator_ / del;
 }
 
-const Rational& Rational::operator+() {
-	return *this;
+void Rational::div(const int number) {
+	denominator_ = denominator_ * number;
+	int del = gcd(numerator_, denominator_);
+	numerator_ = numerator_ / del;
+	denominator_ = denominator_ / del;
 }
 
-const Rational& Rational::operator-() {
-	denominator_ = -denominator_;
-	return *this;
+Rational& Rational::operator+() const {
+	Rational number(this->numerator_, this->denominator_);
+	return number;
+}
+
+Rational& Rational::operator-() const {
+	Rational number(this->numerator_, this->denominator_);
+	number.neg();
+	return number;
 }
 
 Rational& Rational::operator+=(const Rational& another) {
@@ -104,31 +127,38 @@ Rational& Rational::operator+=(const Rational& another) {
 }
 
 Rational& Rational::operator+=(const int another) {
-
+	this->add(another);
+	return *this;
 }
 
 Rational& Rational::operator-=(const Rational& another) {
-
+	this->sub(another);
+	return *this;
 }
 
 Rational& Rational::operator-=(const int another) {
-
+	this->sub(another);
+	return *this;
 }
 
 Rational& Rational::operator*=(const Rational& another) {
-
+	this->mul(another);
+	return *this;
 }
 
 Rational& Rational::operator*=(const int another) {
-
+	this->mul(another);
+	return *this;
 }
 
 Rational& Rational::operator /=(const Rational& another) {
-
+	this->div(another);
+	return *this;
 }
 
 Rational& Rational::operator /=(const int another) {
-
+	this->div(another);
+		return *this;
 }
 
 
@@ -142,12 +172,14 @@ Rational& Rational::operator /=(const int another) {
 
 int main() {
 	std::cout << "HELLO!" << std::endl;
-	Rational number1(10, 1);
-	Rational number2(2, 1);
+	Rational number1(5, 2);
+	Rational number2(7, 2);
+	int a = 4;
 	// number1.add(number2);
 	// number1.sub(number2);
 	// number1.mul(number2);
 	//number1.div(number2);
+	//number1 += number2;
 	number1 += number2;
 	std::cout << "Numerator_ of number1 : " << number1.numerator_ << std::endl;
 	std::cout << "Denominator_ of number1 : " << number1.denominator_ << std::endl;
