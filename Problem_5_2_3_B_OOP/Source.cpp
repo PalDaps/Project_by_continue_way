907e928a81b00214b730bda766d7fc6c786da1f9
Pavel Dashko <dashko.pavel@bk.ru>
2023-07-06 17:22:12 +0300

    slowly I understand the overload of operators

--- Problem_5_2_3_B_OOP/Source.cpp
+++ Problem_5_2_3_B_OOP/Source.cpp
@@ -12,10 +12,26 @@ struct Rational {
 		std::cout << "The destructor volunteered" << std::endl;
 	}
 	void add(Rational number);
+	void add(const int number);
 	void sub(Rational number);
+	void sub(const int number);
 	void mul(Rational number);
+	void mul(const int number);
 	void div(Rational number);
+	void div(const int number);
+	const Rational& operator+();
+	const Rational& operator-();
+	Rational& operator+=(const Rational& another);
+	Rational& operator+=(const int another);
+	Rational& operator-=(const Rational& another);
+	Rational& operator-=(const int another);
+	Rational& operator*=(const Rational& another);
+	Rational& operator*=(const int another);
+	Rational& operator/=(const Rational& another);
+	Rational& operator/=(const int another);
 	int gcd(int a, int b);
+
+
 };
 
 int Rational::gcd(int a, int b) {
@@ -34,6 +50,14 @@ int Rational::gcd(int a, int b) {
 
 
 void Rational::add(Rational number) {
+	numerator_ = numerator_ * number.denominator_ + number.numerator_ * denominator_;
+	denominator_ = denominator_ * number.denominator_;
+	int del = gcd(numerator_, denominator_);
+	numerator_ = numerator_ / del;
+	denominator_ = denominator_ / del;
+}
+
+void Rational::add(const int another) {
 	numerator_ = numerator_ * number.denominator_ + number.numerator_ * denominator_;
 	denominator_ = denominator_ * number.denominator_;
 	int del = gcd(numerator_, denominator_);
@@ -65,16 +89,66 @@ void Rational::div(Rational number) {
 	denominator_ = denominator_ / del;
 }
 
+const Rational& Rational::operator+() {
+	return *this;
+}
+
+const Rational& Rational::operator-() {
+	denominator_ = -denominator_;
+	return *this;
+}
+
+Rational& Rational::operator+=(const Rational& another) {
+	this->add(another);
+	return *this;
+}
+
+Rational& Rational::operator+=(const int another) {
+
+}
+
+Rational& Rational::operator-=(const Rational& another) {
+
+}
+
+Rational& Rational::operator-=(const int another) {
+
+}
+
+Rational& Rational::operator*=(const Rational& another) {
+
+}
+
+Rational& Rational::operator*=(const int another) {
+
+}
+
+Rational& Rational::operator /=(const Rational& another) {
+
+}
+
+Rational& Rational::operator /=(const int another) {
+
+}
+
+
+
+
+
+
+
+
 
 
 int main() {
 	std::cout << "HELLO!" << std::endl;
-	Rational number1(5, 1);
+	Rational number1(10, 1);
 	Rational number2(2, 1);
 	// number1.add(number2);
 	// number1.sub(number2);
 	// number1.mul(number2);
-	number1.div(number2);
+	//number1.div(number2);
+	number1 += number2;
 	std::cout << "Numerator_ of number1 : " << number1.numerator_ << std::endl;
 	std::cout << "Denominator_ of number1 : " << number1.denominator_ << std::endl;
 	return 0;
